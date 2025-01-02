8dcaa79d0337fb4a751f93ed51504185652552e6
SampiNova <dmitry.karpachev@hotmail.com>
2025-01-02 20:06:07 +0700

    new

--- lua/text/vim-options.lua
+++ lua/text/vim-options.lua
@@ -30,13 +30,5 @@ vim.api.nvim_set_keymap('n', ',h', '<cmd>bp<CR>',{})
 vim.api.nvim_set_keymap('n', ',l', '<cmd>bn<CR>',{})
 vim.api.nvim_set_keymap('n', ',c', '<cmd>bd<CR>',{})
 
---Easy Align
-vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {noremap = false})
-vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {noremap = false})
-
---Translator
-vim.api.nvim_set_keymap('v', '<leader>p', '<cmd>Translate ru<CR>', {})
-vim.api.nvim_set_keymap('v', '<leader>pa', '<cmd>Translate en -output=replace<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>pw', 'viw<cmd>Translate en -output=replace<CR><ESC>', {})
 
 
